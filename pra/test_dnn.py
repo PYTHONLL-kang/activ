44cd6f33f58bcc67bb33a67c28bb7360003b264a
PTYTHONLL-kang <dlrkd0987@gmail.com>
2022-07-17 00:14:57 +0900

    add remark

--- pra/test_dnn.py
+++ pra/test_dnn.py
@@ -8,25 +8,27 @@ import matplotlib.pyplot as plt
 from tensorflow.python.keras.models import load_model
 
 df = pd.read_excel('dataframe.xlsx')
-X = df.iloc[:,0:5]
-y = df.iloc[:,5]
-scaler = StandardScaler()   
-X_norm = scaler.fit_transform(X)
+#메인 변수 5개 - 소비자물가, 금리, 국내총생산, 신생아 출산인구, 합계 출산율
+#Y값(레이블) - 수도권 인구 과밀화율
+X = df.iloc[:,0:5] #0~5까지 열, 즉 메인변수들
+y = df.iloc[:,5] #마지막 열, 즉 레이블
+scaler = StandardScaler() #표준화. 평균이 0이고 분산이 1인 정규분포로 만드는 것
+X_norm = scaler.fit_transform(X) #메인 변수들끼리 단위가 모두 다르기 때문에 표준화 시켜야 함.
 
-numpY = np.empty((278,1)) 
+numpY = np.empty((278,1)) #pandas라서 numpy형으로 바꿈
 for i in range(278):
     numpY[i]=y[i]
 
-# training set과 test set으로 나누기
+#training set과 test set으로 나누기 #랜덤으로 스플릿
 X_train,X_test,y_train,y_test=train_test_split(X_norm, numpY, test_size=0.2, random_state=42,shuffle=True)
 
 # 모델 구조 정의하기
 model = tf.keras.Sequential()  
 
 #입력 8개로부터 전달받는 12개 노드의 layer 생성
-model.add(layers.Dense(128, input_shape=(5,),activation='sigmoid'))  
-model.add(layers.Dense(64,activation='relu'))
-model.add(layers.Dense(16,activation='relu'))         
+model.add(layers.Dense(128, input_shape=(5,),activation='sigmoid')) #메인변수 5개라서 input shape = 5. 활성함수 sigmoid로 
+model.add(layers.Dense(64,activation='relu')) #활성함수 relu
+model.add(layers.Dense(16,activation='relu')) #활성함수 relu
 #회귀모형(regression) 구축을 위해서 linear 활성함수 사용
 model.add(layers.Dense(1,activation='linear')) 
 model.summary()
@@ -42,16 +44,19 @@ hist = model.fit(
     batch_size=10,    
     epochs=10000,       
     validation_split=0.2,  
-    callbacks=[tf.keras.callbacks.EarlyStopping(monitor='mae', patience=100)],  
-    verbose=2)
+    callbacks=[tf.keras.callbacks.EarlyStopping(monitor='mae', patience=1)], #과적합 방지용. loss가 100 epoch 동안 개선되지 않으면 학습 중단 
+    verbose=2) #학습 중 출력 문구 설정. 0이면 출력 X, 1이면 훈련 진행 막대, 2이면 미니배치마다 loss
+
 # 모델 저장
-model.save("test_dnn.h5")
-scores = model.evaluate(X_test, y_test)
+model.save("test_dnn.h5") #test_dnn.h5라는 이름으로 모델 저장
+
+scores = model.evaluate(X_test, y_test) #분리해둔 테스트 데이터로 모델 평가 후 scores 변수에 저장
 
 # 관찰된 metric 값들을 확인함
 for i in range(len(scores)):
     print("%s: %.2f" % (model.metrics_names[i], scores[i]))
 
+#모델 손실 그래프 준비
 fig, loss_ax = plt.subplots(figsize=(15, 5))
 
 acc_ax = loss_ax.twinx()
