84f6ea1d6a68d4c8e868dc1f173dddecfa7fe5a3
PTYTHONLL-kang <dlrkd0987@gmail.com>
2022-07-20 15:27:05 +0900

    ddd

--- pra/test_lstm.py
+++ pra/test_lstm.py
@@ -1,3 +1,6 @@
+#https://insightcampus.co.kr/2021/11/11/%EC%8B%9C%EA%B3%84%EC%97%B4-%EC%98%88%EC%B8%A1-lstm-%EB%AA%A8%EB%8D%B8%EB%A1%9C-%EC%A3%BC%EA%B0%80-%EC%98%88%EC%B8%A1%ED%95%98%EA%B8%B0/
+
+
 import math
 import matplotlib.pyplot as plt
 import keras
@@ -27,8 +30,6 @@ sc = MinMaxScaler(feature_range = (0, 1))
 
 training_set_scaled = sc.fit_transform(training_set)
 
-# Creating a data structure with 60 time-steps and 1 output
-
 X_train = []
 y_train = []
 
@@ -41,82 +42,48 @@ X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
 
 model = Sequential()
 
-#Adding the first LSTM layer and some Dropout regularisation
-
 model.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
-
 model.add(Dropout(0.2))
-
-# Adding a second LSTM layer and some Dropout regularisation
-
 model.add(LSTM(units = 50, return_sequences = True))
-
 model.add(Dropout(0.2))
-
-# Adding a third LSTM layer and some Dropout regularisation
-
 model.add(LSTM(units = 50, return_sequences = True))
-
 model.add(Dropout(0.2))
-
-# Adding a fourth LSTM layer and some Dropout regularisation
-
 model.add(LSTM(units = 50))
-
 model.add(Dropout(0.2))
-
-# Adding the output layer
-
 model.add(Dense(units = 1))
-
-# Compiling the RNN
-
-model.compile(optimizer = ‘adam’, loss = ‘mean_squared_error’)
-
-# Fitting the RNN to the Training set
+model.compile(optimizer = "adam", loss = "‘mean_squared_error’")
 
 model.fit(X_train, y_train, epochs = 100, batch_size = 32)
 
 dataset_train = df.iloc[:800, 1:2]
-
 dataset_test = df.iloc[800:, 1:2]
 
 dataset_total = pd.concat((dataset_train, dataset_test), axis = 0)
 
 inputs = dataset_total[len(dataset_total) – len(dataset_test) – 60:].values
-
 inputs = inputs.reshape(-1,1)
-
 inputs = sc.transform(inputs)
 
 X_test = []
 
 for i in range(60, 519):
-
    X_test.append(inputs[i-60:i, 0])
 
 X_test = np.array(X_test)
-
 X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
 
 print(X_test.shape)
 
 predicted_stock_price = model.predict(X_test)
-
 predicted_stock_price = sc.inverse_transform(predicted_stock_price)
 
-plt.plot(df.loc[800:, ‘Date’],dataset_test.values, color = ‘red’, label = ‘Real TESLA Stock Price’)
-
-plt.plot(df.loc[800:, ‘Date’],predicted_stock_price, color = ‘blue’, label = ‘Predicted TESLA Stock Price’)
+plt.plot(df.loc[800:, "Date"], dataset_test.values, color = "red", label = "Real TESLA Stock Price")
+plt.plot(df.loc[800:, "Date"],predicted_stock_price, color = "blue", label = "Predicted TESLA Stock Price’")
 
 plt.xticks(np.arange(0,459,50))
+plt.title("TESLA Stock Price Prediction")
 
-plt.title(‘TESLA Stock Price Prediction’)
-
-plt.xlabel(‘Time’)
-
-plt.ylabel(‘TESLA Stock Price’)
-
+plt.xlabel("Time")
+plt.ylabel("TESLA Stock Price")
 plt.legend()
-
 plt.show()
